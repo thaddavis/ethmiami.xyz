07fbf64c6a6f77cc204a69b67471b6c284cb6d26
Julio M Cruz <julio.cruz@eb-ms.net>
2023-04-02 04:50:16 -0400

    Crossmint NFT to Assests

--- src/components/Artworks.jsx
+++ src/components/Artworks.jsx
@@ -105,8 +105,8 @@ const Card = ({ nft }) => {
 
           <CrossmintPayButton
           className="payButton"
-          clientId="80c3f288-e48b-4fb6-a7dc-3d43ca029451"
-          mintConfig={{"type":"thirdweb-drop","totalPrice":"0.001","quantity":"1"}}
+          clientId={nft.crossmint_clientId}
+          mintConfig={{"type":`${nft.crossmint_type}`,"totalPrice":`${nft.crossmint_totalPrice}`,"quantity":`${nft.crossmint_quantity}`}}
           environment="staging"
         />
 
